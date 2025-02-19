4d6ba74ef6220796af1a9ab457c391d5e0faea04
rollout147 <rollout147@gmail.com>
2025-02-19 16:59:16 +0900

    20250219 second commit

---
+++ src/main/java/com/oracle/todoList/model/User_table.java
@@ -0,0 +1,11 @@
+package com.oracle.todoList.model;
+
+import lombok.Data;
+
+@Data
+public class User_table {
+	private String	user_id;
+	private String	user_pw;
+	private String	user_name;
+	private String	user_email;
+}
